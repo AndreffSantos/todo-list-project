11a2d7da761b66734d229c57d932b9b718f4abda
Andre Santos <andrefelipe_santos@outlook.com>
2021-12-12 00:29:54 -0300

    Requisito 12 :heavy_check_mark:

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const buttonCriarTarefa = document.querySelector('#criar-tarefa');
 const olListaTarefas = document.querySelector('#lista-tarefas');
 const buttonApagarTarefas = document.querySelector('#apaga-tudo');
 const buttonRemoverFinalizados = document.querySelector('#remover-finalizados');
+const buttonSalvarTarefas = document.querySelector('#salvar-tarefas');
+const string1 = 'CONTENT-';
+const string2 = 'CLASS-';
 // Todas as funções do script.
 
 function adicionaTarefa() {
@@ -32,6 +35,7 @@ function riscarTarefa(event) {
 
 function apagarLista() {
   olListaTarefas.innerHTML = '';
+  localStorage.clear();
 }
 
 function removerFinalizados() {
@@ -41,9 +45,43 @@ function removerFinalizados() {
   }
 }
 
+function capturaLista(lista) {
+  for (let index = 0; index < lista.length; index += 1) {
+    const valor1 = string1.concat(index);
+    const valor2 = string2.concat(index);
+    localStorage.setItem(valor1, lista[index].textContent);
+    localStorage.setItem(valor2, lista[index].className);
+  }
+}
+
+function salvarLista() {
+  const lista = document.querySelectorAll('li');
+  if (document.querySelector('.selected') !== null) {
+    document.querySelector('.selected').classList.toggle('selected');
+    capturaLista(lista);
+  } else {
+    capturaLista(lista);
+  }
+}
+
 // Chama todas as funções
 buttonCriarTarefa.addEventListener('click', adicionaTarefa);
 olListaTarefas.addEventListener('click', selecionaTarefa);
 olListaTarefas.addEventListener('dblclick', riscarTarefa);
 buttonApagarTarefas.addEventListener('click', apagarLista);
 buttonRemoverFinalizados.addEventListener('click', removerFinalizados);
+buttonSalvarTarefas.addEventListener('click', salvarLista);
+
+const tam = localStorage.length;
+if (tam > 0) {
+  for (let index = 0; index < tam / 2; index += 1) {
+    const newLi = document.createElement('li');
+    const valor1 = string1.concat(index);
+    newLi.innerText = localStorage[valor1];
+    const valor2 = string2.concat(index);
+    if (localStorage[valor2] !== '') {
+      newLi.className = localStorage[valor2];
+    }
+    olListaTarefas.appendChild(newLi);    
+  }
+}
