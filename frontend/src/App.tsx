ea8bde9e95825bf6cb5f30c0ddbf6e37f4b3fd94
Andre <andrefelipe_santos@outlook.com>
2023-03-24 00:03:20 -0300

    ✨ feat(Implementação de rotas para as paginas):

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
-import GetStarted from './pages/GetStarted'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import AddTodo from './pages/Todo'
+import './index.css'
 
-function App() {
+export default function App() {
   return (
-    <div className='App'>
-      Iniciando projeto
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Navigate to="/home" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/Signup' element={<Signup />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/AddTodo' element={<AddTodo />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
