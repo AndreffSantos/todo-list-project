96198cba2d9d27daac741af35c98b2ad864436af
Andre <andrefelipe_santos@outlook.com>
2023-04-06 18:32:40 -0300

    ✨ feat(manipulação do banco dados):

--- backend/src/db/DB.py
+++ backend/src/db/DB.py
@@ -15,8 +15,33 @@ def conect():
 def get_user(email: str, password: str):
 	query = f'select * from users where email="{email}" and password="{password}";'
 	db = conect()
-	cursor = db.cursor()
+	cursor = db.cursor(dictionary=True)
+	cursor.execute(query)
+	result = cursor.fetchone()
+	db.close()
+	return result
+
+def insert_user(name: str, email: str, password: str):
+	query = f'insert into users (name, email, password) values ("{name}", "{email}", "{password}")'
+	db = conect()
+	cursor = db.cursor(dictionary=True)
+	cursor.execute(query)
+	db.commit()
+
+def get_tasks_by_user(id: int):
+	query = f'select * from tasks where user_id={id}'
+	db = conect()
+	cursor = db.cursor(dictionary=True)
 	cursor.execute(query)
 	result = cursor.fetchall()
 	db.close()
-	return result[0]
+	return result
+
+def insert_task(id: str, description: str):
+	query = f'insert into tasks (user_id, description) values ("{id}", "{description}")'
+	db = conect()
+	cursor = db.cursor(dictionary=True)
+	cursor.execute(query)
+	db.commit()
+	db.close()
+	
