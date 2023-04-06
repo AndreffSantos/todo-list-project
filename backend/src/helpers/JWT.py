aa7b0cc90e38691db3e0226fa3f5e48107c6744d
Andre <andrefelipe_santos@outlook.com>
2023-04-06 18:34:58 -0300

    ✨ feat(gerar tokens e validação de token.):

--- backend/src/helpers/JWT.py
+++ backend/src/helpers/JWT.py
@@ -4,5 +4,8 @@ from dotenv import load_dotenv, dotenv_values
 load_dotenv()
 env = dotenv_values('.env')
 
-def token(payload: dict):
-    return jwt.encode(payload, env['SECRET'], algorithm='HS256')
+def encode(payload: dict):
+    return jwt.encode(payload, env['SECRET'], headers={ 'alg': 'HS256', 'exp': 1 })
+
+def decode(token: str):
+    return jwt.decode(token, env['SECRET'], algorithms=['HS256'])
